a3fee6e3dac5151df739559efa25e78dace9b4bd
igopichandcse2405v0-stack <igopichand_cse2405vo@mgit.ac.in>
2026-09-10 20:51:55 +0530

    Add reset progress function

--- js/app.js
+++ js/app.js
@@ -33,3 +33,10 @@ document.getElementById("score").innerHTML =
 
 document.getElementById("completed").innerHTML =
     savedCompleted;
+function resetProgress() {
+    localStorage.setItem("ecoScore", 0);
+    localStorage.setItem("completedHabits", 0);
+
+    document.getElementById("score").innerHTML = 0;
+    document.getElementById("completed").innerHTML = 0;
+}
